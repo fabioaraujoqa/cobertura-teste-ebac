2f63ee9c9e1f24634999411df094856651491847
fabioaraujoqa <fabio.araujo.qa@gmail.com>
2024-05-09 18:39:16 -0300

    template

---
+++ src/desconto.js
@@ -0,0 +1,13 @@
+function calcDesconto(quantidade) {
+    if (quantidade <= 10) {
+        return 0;  // sem desconto
+    } else if (quantidade <= 20) {
+        return 0.10;  // 10% de desconto
+    } else if (quantidade <= 50) {
+        return 0.25;  // 25% de desconto
+    } else {
+        return 0.35;  // 35% de desconto
+    }
+}
+
+module.exports = calcDesconto;
---
+++ test/desconto.test.js
@@ -0,0 +1,19 @@
+import desconto from '../src/desconto'
+
+describe('Cálculo de Desconto Baseado na Quantidade', () => {
+    it('Deve aplicar 0% de desconto para até 10 produtos', () => {
+        expect(desconto(5)).toBe(0);
+    });
+
+    it('Deve aplicar 10% de desconto para de 11 a 20 produtos', () => {
+        expect(desconto(15)).toBe(0.10);
+    });
+
+    it('Deve aplicar 25% de desconto para de 21 a 50 produtos', () => {
+        expect(desconto(30)).toBe(0.25);
+    });
+
+    it('Deve aplicar 35% de desconto para mais de 50 produtos', () => {
+        expect(desconto(51)).toBe(0.35);
+    });
+});
